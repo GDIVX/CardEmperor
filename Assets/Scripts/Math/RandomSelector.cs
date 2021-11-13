410701bf3cccb6016f776cb5126d1f3a1ffe22ba
Yarin Vardimon <Yarin107@gmail.com>
2021-11-13 21:09:41 +0200

    added random selector

---
+++ Assets/Scripts/Math/RandomSelector.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class RandomSelector : MonoBehaviour
+{
+    [OnValueChanged("calculateValues")]
+    [Range(0,1)]
+    public float commonRarity = .80f;
+    [OnValueChanged("calculateValues")]
+    [Range(0,1)]
+    public float uncommonRarity = .20f;
+    [OnValueChanged("calculateValues")]
+    [Range(-1,1)]
+    public float rareRarity;
+
+    float saveCommon = 0;
+
+
+    void Start()
+    {
+        calculateValues();
+    }
+    void calculateValues(){
+        rareRarity = 1 - (commonRarity + uncommonRarity);
+        if(saveCommon == 0) saveCommon = commonRarity;
+    }
+
+    public enum Rarity{ COMMON , UNCOMMON , RARE}
+
+    public Rarity GetRandomRarity(){
+        float rand = Random.value;
+        if(rand <= rareRarity) return Rarity.RARE;
+        if(rand <= uncommonRarity) return Rarity.UNCOMMON;
+        return Rarity.COMMON;
+    }
+
+    public Rarity GetRarityAndAdjust(){
+        Rarity res = GetRandomRarity();
+        if(res == Rarity.RARE){
+            commonRarity = saveCommon;
+        }
+        else if(commonRarity > 0.10f){
+            commonRarity -= .05f;
+        }
+        else{
+            uncommonRarity -=0.05f;
+        }
+        calculateValues();
+        return res;
+    } 
+
+    [Button]
+    void test(){
+        Debug.Log(GetRarityAndAdjust());
+    }
+}
